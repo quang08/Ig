67f828bb4c5bacc330cd87e4572d9bba5509cffb
Quang Nguyen The <quangnguyenthe@Quangs-MacBook-Air.local>
2023-01-02 10:37:41 +0700

    added left icons for header and finished header sticky top

--- components/Header.js
+++ components/Header.js
@@ -9,10 +9,13 @@ import {
   MenuIcon,
 } from "@heroicons/react/outline";
 
+import { HomeIcon } from "@heroicons/react/solid";
+import ppf from "../public/3.png";
+
 function Header() {
   return (
-    <div>
-      <div className="flex justify-between max-w-6xl">
+    <div className="border-b border-gray-300 bg-white sticky top-0 z-50">
+      <div className="flex justify-between max-w-6xl mx-5 lg:mx-auto">
         {/* Left */}
         <div className="relative hidden lg:inline-grid w-24 cursor-pointer">
           <Image
@@ -31,14 +34,38 @@ function Header() {
         </div>
 
         {/* Left */}
-        <div className="relative mt-1 p-3 rounded-md">
-          <div className="absolute inset-y-0 pl-3 flex items-center pointer-events-none">
-            <SearchIcon className="text-gray-400 h-5 w-5" />
+        <div className="max-w-xs">
+          <div className="relative mt-1 p-3 rounded-md">
+            <div className="absolute inset-y-0 pl-3 flex items-center pointer-events-none">
+              <SearchIcon className="text-gray-400 h-5 w-5" />
+            </div>
+            <input
+              className="block w-full pl-10 bg-gray-50 sm:text-sm border-gray-300 focus:ring-black focus:border-black rounded-md"
+              type="text"
+              placeholder="Search"
+            />
           </div>
-          <input
-            className="block w-full pl-10 bg-gray-50 sm:text-sm border-gray-300 focus:ring-black focus:border-black rounded-md"
-            type="text"
-            placeholder="Search"
+        </div>
+
+        {/* Right */}
+        <div className="flex items-center justify-end space-x-4">
+          <HomeIcon className="navBtn" />
+          <MenuIcon className="h-8 md:hidden cursor-pointer text-black" />
+
+          <div className="relative navBtn">
+            <PaperAirplaneIcon className="navBtn rotate-45" />
+            <div className="absolute -top-2 -right-2 text-xs w-5 h-5 text-white bg-red-500 rounded-full flex items-center justify-center">
+              3
+            </div>
+          </div>
+
+          <PlusCircleIcon className="navBtn" />
+          <UserGroupIcon className="navBtn" />
+          <HeartIcon className="navBtn" />
+          <Image
+            src={ppf}
+            alt="profile pic"
+            className="h-10 w-10 rounded-full cursor-pointer"
           />
         </div>
       </div>
